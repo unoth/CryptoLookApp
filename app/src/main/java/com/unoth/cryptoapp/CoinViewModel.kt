bb89c0d858a4ae2e4f1aea2840558402119883cb
Kristina Dedova <dan005bk@gmail.com>
2024-05-07 00:38:54 +0300

    feat(CoinViewModel): add and configure ViewModel

---
+++ app/src/main/java/com/unoth/cryptoapp/CoinViewModel.kt
@@ -0,0 +1,59 @@
+package com.unoth.cryptoapp
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.google.gson.Gson
+import com.unoth.cryptoapp.api.ApiFactory
+import com.unoth.cryptoapp.database.AppDatabase
+import com.unoth.cryptoapp.pojo.CoinPriceInfo
+import com.unoth.cryptoapp.pojo.CoinPriceInfoRawData
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = AppDatabase.getInstance(application)
+    val priceList = db.coinPriceInfoDao().getPriceList()
+
+    private val compositeDisposable = CompositeDisposable()
+    fun loadData() {
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .map { it.data?.map { it.coinInfo?.name }?.joinToString(",").toString() }
+            .flatMap { ApiFactory.apiService.getFullPriceList(fSyms = it) }
+            .map { getPriceListFromRawData(it) }
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                db.coinPriceInfoDao().insertPriseList(it)
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    private fun getPriceListFromRawData(coinPriceInfoRawData: CoinPriceInfoRawData): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject
+        if (jsonObject == null) return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
--- app/src/main/java/com/unoth/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/unoth/cryptoapp/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: CoinViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
     }
+
+
 }
