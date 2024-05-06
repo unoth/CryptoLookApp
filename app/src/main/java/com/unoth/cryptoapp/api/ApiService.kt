afb28ea8e50a6d032fca687051162d37cc9a3113
Kristina Dedova <dan005bk@gmail.com>
2024-05-06 22:08:52 +0300

    fix: refactoring const

--- app/src/main/java/com/unoth/cryptoapp/api/ApiService.kt
+++ app/src/main/java/com/unoth/cryptoapp/api/ApiService.kt
@@ -9,19 +9,21 @@ import retrofit2.http.Query
 interface ApiService {
     @GET("top/totalvolfull")
     fun getTopCoinsInfo(
-        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
         @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
         @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
     ): Single<CoinInfoListOfData>
 
     @GET("pricemultifull")
     fun getFullPriceList(
-        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
         @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
         @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
     ): Single<CoinPriceInfoRawData>
 
     companion object {
+        private const val API_KEY =
+            "d24c3890832858173415133c9bdf34f520933084c5ffe091fbc4e9bcc041966a"
         private const val QUERY_PARAM_API_KEY = "api_key"
         private const val QUERY_PARAM_LIMIT = "limit"
         private const val QUERY_PARAM_TO_SYMBOL = "tsym"
